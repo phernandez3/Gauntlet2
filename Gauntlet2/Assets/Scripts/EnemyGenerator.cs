b1b2b59624a0cfc91d1b99313bfc59ed2ae577e5
eswartz123 <eswartz@mail.csuchico.edu>
2021-05-06 13:56:34 -0700

    EnemyGenerator work in progress

--- Gauntlet2/Assets/Scripts/EnemyGenerator.cs
+++ Gauntlet2/Assets/Scripts/EnemyGenerator.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class EnemyGenerator : BaseUnit
 {
+    [SerializeField] private List<GameObject> enemiesAlive = new List<GameObject>();     //Tracks living enemies for this generator
+    [SerializeField] private List<GameObject> enemiesDead = new List<GameObject>();      //Tracks when an enemy has died
+
+    [SerializeField] private int spawnLimit;             //Sets a capacity for how many enemies can be spawned
+    [SerializeField] private Transform spawnPoint;       //Where enemies will appear from once spawned
     [SerializeField] private GameObject enemyPrefab;     //Enemy type for THIS enemy generator goes here
 
 
     private void Awake()
     {
         healthPoints = 5;
+    }
+
+
+    private void Update()
+    {
+        
     }
 
 
@@ -20,13 +31,23 @@ public class EnemyGenerator : BaseUnit
         {
             if(hit.collider != null)
             {
-                Debug.Log("spawn enemy");
+                Debug.Log("being blocked");
+                return;
             }
-            Debug.Log("being blocked");
         }
+        Debug.Log("enemy spawned");
     }
 
 
+    private void EnemySpawn()
+    {
+        if(spawnLimit < enemiesAlive.Count)
+        {
+            GameObject gObjEnemy = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity) as GameObject;
+            enemiesAlive.Add(gObjEnemy);
+            spawnLimit++;
+        }
+    }
     //need to check adjacent spaces to generator (raycast)
     //coroutine spawning
 }
