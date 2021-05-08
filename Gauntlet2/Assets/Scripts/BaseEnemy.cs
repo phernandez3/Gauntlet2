f9886689d73f0534e614311ae7b672748aef8b11
eswartz123 <eswartz@mail.csuchico.edu>
2021-05-07 19:20:06 -0700

    EnemyGenerator done, BaseEnemy in progress

---
+++ Gauntlet2/Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : BaseUnit
+{
+    //integrate findobjectsoftype into fixed array[]
+    public GameObject[] allPlayers;
+
+    public void Update()
+    {
+        FindClosestPlayer();
+    }
+
+
+    public void FindClosestPlayer()
+    {
+        float distanceToClosestPlayer = Mathf.Infinity;
+        
+        //BasePlayer closestPlayer = null;
+        //BasePlayer[] allPlayers = GameObject.FindObjectsOfType<BasePlayer>();
+
+        foreach(GameObject currentPlayer in allPlayers)
+        {
+            float distanceToPlayer = (currentPlayer.transform.position - this.transform.position).sqrMagnitude;
+            if(distanceToPlayer < distanceToClosestPlayer)
+            {
+                distanceToClosestPlayer = distanceToPlayer;
+                closestPlayer = currentPlayer;
+            }
+        }
+        Debug.DrawLine(this.transform.position, closestPlayer.transform.position);
+    }
+}
--- Gauntlet2/Assets/Scripts/BaseUnit.cs
+++ Gauntlet2/Assets/Scripts/BaseUnit.cs
@@ -6,14 +6,15 @@ public class BaseUnit : MonoBehaviour
 {
     public int healthPoints;
     public int moveSpeed;
+    public bool isDead = false;
 
 
     public void TakeDamage(int damageAmount)
     {
-        //healthPoints -= damageAmount;
-        //if(healthPoints <= 0)
-        //{
-        //    Destroy(this);
-        //}
+        healthPoints -= damageAmount;
+        if (healthPoints <= 0)
+        {
+            Destroy(this);
+        }
     }
 }
