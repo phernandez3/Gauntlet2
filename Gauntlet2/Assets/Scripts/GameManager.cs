8353070631b2303b5213863142707f3558895b2f
Jackie Crosswhite <jcrosswhite@mail.csuchico.edu>
2021-05-10 18:52:29 -0700

    5/10 meeting

--- Gauntlet2/Assets/Scripts/GameManager.cs
+++ Gauntlet2/Assets/Scripts/GameManager.cs
@@ -20,21 +20,27 @@ public class GameManager : MonoBehaviour
         }
     }
 
-
-    [SerializeField] private GameState myGameState;
-    private UI_Manager myUI;
+    public GameState myGameState;
+    public UI_Manager myUI;
     //private AudioManager myAudio;
 
     // Determine what players are in play. If the play is not in play remove references to their UI/system.
-    private bool p1_isPlaying;
-    private bool p2_isPlaying;
-    private bool p3_isPlaying;
-    private bool p4_isPlaying;
+    public bool p1_isPlaying;
+    public bool p2_isPlaying;
+    public bool p3_isPlaying;
+    public bool p4_isPlaying;
 
-    public int p1_Score;
-    public int p2_Score;
-    public int p3_Score;
-    public int p4_Score;
+    public int p1_score;
+    public int p2_score;
+    public int p3_score;
+    public int p4_score;
+    public int total_score;
+    public int high_score;
+
+
+    public int currentLevel;
+    public List<GameObject> levelGeometry = new List<GameObject>();
+
 
 
     void Awake()
--- Gauntlet2/Assets/Scripts/UI_Manager.cs
+++ Gauntlet2/Assets/Scripts/UI_Manager.cs
@@ -5,17 +5,69 @@ using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour
 {
-    private Text p1_Score_UI;
-    private Text p2_Score_UI;
-    private Text p3_Score_UI;
-    private Text p4_Score_UI;
+    private Text announcement_UI;
 
+    private Text p1_isPlaying_UI;
+    private Text p2_isPlaying_UI;
+    private Text p3_isPlaying_UI;
+    private Text p4_isPlaying_UI;
 
-    void Update_UI()
+    private Text p1_score_UI;
+    private Text p2_score_UI;
+    private Text p3_score_UI;
+    private Text p4_score_UI;
+
+    private Image p1_HP_UI;
+    private Image p2_HP_UI;
+    private Image p3_HP_UI;
+    private Image p4_HP_UI;
+
+    private Image p1_item_UI;
+    private Image p2_item_UI;
+    private Image p3_item_UI;
+    private Image p4_item_UI;
+
+    public void Awake()
     {
-        p1_Score_UI.text = GameManager.Instance.p1_Score.ToString();
-        p2_Score_UI.text = GameManager.Instance.p2_Score.ToString();
-        p3_Score_UI.text = GameManager.Instance.p3_Score.ToString();
-        p4_Score_UI.text = GameManager.Instance.p4_Score.ToString();
+        p1_isPlaying_UI = GameObject.Find("p1_isPlaying").GetComponent<Text>();
+        p2_isPlaying_UI = GameObject.Find("p2_isPlaying").GetComponent<Text>();
+        p3_isPlaying_UI = GameObject.Find("p3_isPlaying").GetComponent<Text>();
+        p4_isPlaying_UI = GameObject.Find("p4_isPlaying").GetComponent<Text>();
+
+        p1_score_UI = GameObject.Find("p1_score#").GetComponent<Text>();
+        p2_score_UI = GameObject.Find("p2_score#").GetComponent<Text>();
+        p3_score_UI = GameObject.Find("p3_score#").GetComponent<Text>();
+        p4_score_UI = GameObject.Find("p4_score#").GetComponent<Text>();
+    }
+    public void TurnOffUI()
+    {
+        announcement_UI.enabled = false;
+    }
+    void Update_Announcement()
+    {
+
+    }
+
+    void Update_Roster()
+    {
+
+    }
+    void Update_Score()
+    {
+
+        p1_score_UI.text = GameManager.Instance.p1_score.ToString();
+        p2_score_UI.text = GameManager.Instance.p2_score.ToString();
+        p3_score_UI.text = GameManager.Instance.p3_score.ToString();
+        p4_score_UI.text = GameManager.Instance.p4_score.ToString();
+    }
+
+    void Update_HP()
+    {
+        
+    }
+
+    void Update_Item()
+    {
+
     }
 }
